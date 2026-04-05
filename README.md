8945ea2ba25af10b033f23023e8931249137491b
FoxyWoxy12000 <140989615+FoxyWoxy12000@users.noreply.github.com>
2026-04-05 05:05:26 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lazytntview-1.21.11-1.21.10
+A mod for the veiwing of tnt entities inside of lazy chunks
---
+++ src/client/java/net/orbitalstrike/client/LazyTNTviewClient.java
@@ -0,0 +1,10 @@
+package net.orbitalstrike.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LazyTNTviewClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
